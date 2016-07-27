3a31c57ed820a0e7dabd194c6cb0ab70b1c42113
Radek Slupik <rightfold@gmail.com>
2016-07-27 10:42:31 +0200

    Doc comments

--- src/Control/Eval.purs
+++ src/Control/Eval.purs
@@ -11,6 +11,7 @@ import Data.Lazy (Lazy)
 import Data.Lazy as Lazy
 import Prelude
 
+-- | Evaluation strategy.
 class Eval f where
   defer :: forall a. (Unit -> a) -> f a
   force :: forall a. f a -> a
@@ -19,12 +20,14 @@ instance evalLazy :: Eval Lazy where
   defer = Lazy.defer
   force = Lazy.force
 
+-- | Strict evaluation. `defer` immediately calls the thunk.
 newtype Strict a = Strict a
 
 instance evalStrict :: Eval Strict where
   defer t = Strict (t unit)
   force (Strict x) = x
 
+-- | By-name evaluation. `force` calls the thunk every time.
 newtype Replay a = Replay (Unit -> a)
 
 instance evalReplay :: Eval Replay where
